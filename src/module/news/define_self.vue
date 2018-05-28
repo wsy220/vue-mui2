f965bd51067af722baa9c6b082e37557fc99de30
35551400@qq.com <fish1981220>
2018-05-28 21:24:44 +0800

    dd

---
+++ src/module/news/define_self.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <div id="newsApp">
+    <header class="mui-bar mui-bar-nav">
+      <!--<a class="mui-action-menu mui-icon mui-icon-bars mui-pull-left"></a>-->
+      <h1 id="title" class="mui-title">消息</h1>
+      <!--<a class="mui-icon mui-pull-right" id="loginBth"><i class="fa fa-user-circle-o" aria-hidden="true"></i></a>-->
+    </header>
+
+
+    <custom-input v-model="price"></custom-input>
+
+
+
+
+  </div>
+</template>
+
+
+<script>
+  import Vue from "vue"
+
+  export default {
+
+    data() {
+      return {
+        data:{price:''}
+      }
+    }
+  }
+  Vue.component('custom-input', {
+    props: ['value'],
+    template: `
+    <input
+      v-bind:value="value"
+      v-on:input="updateValue($event.target.value)"
+    >
+  `,
+    methods:{
+      updateValue:function (value) {
+        var formattedValue=value.trim();
+        if(formattedValue!==value){
+          this.$refs.input.value=formattedValue;
+        }
+        this.$emit('input',Number(formattedValue));
+      }
+    }
+  })
+
+</script>
+
+<style scoped>
+
+</style>
